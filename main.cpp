7587aa6475d6bdf3f4635d4540949701a951c57a
WLasota <36998871+Arakis14@users.noreply.github.com>
2022-02-15 22:17:56 +0100

    First iteration of setUpLetters

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <vector>
 #include <memory>
 #include "src/CBlankTile.cpp"
 #include "src/CLetter.cpp"
 #include "src/Alphabet.hpp"
 
+void setUpLetters(std::shared_ptr<std::vector<CLetter>> collection)
+{
+    for (auto i = 0; i < 9; i++)
+    {
+        collection->emplace_back(alphabet::a);
+    }
+        for (auto i = 0; i < 2; i++)
+    {
+        collection->emplace_back(alphabet::b);
+    }
+        for (auto i = 0; i < 2; i++)
+    {
+        collection->emplace_back(alphabet::c);
+    }
+        for (auto i = 0; i < 4; i++)
+    {
+        collection->emplace_back(alphabet::d);
+    }
+    for (auto i = 0; i < 12; i++)
+    {
+        collection->emplace_back(alphabet::e);
+    }
+}
 
 int main()
 {
-    std::cout << "Project!" << std::endl;
-    //alphabet abc;
-    //abc = alphabet::q;
-    std::shared_ptr <CLetter> letter1 = std::make_shared<CLetter>(alphabet::w);
-
-    std::cout << letter1->getPoints() << std::endl;
+    auto collection = std::make_shared<std::vector<CLetter>>();
+    setUpLetters(collection);
+    std::cout << collection->size() << std::endl;
 }
